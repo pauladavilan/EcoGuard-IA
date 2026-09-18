e17a3dbdbac7910b354d6e3436448b9468cd91e9
pauladavilan <pauladavilan@gmail.com>
2026-09-18 02:38:44 -0600

    Update app.py

--- app.py
+++ app.py
@@ -42,8 +42,7 @@ with col1:
     
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
-        st.image(image, caption="Imagen cargada", use_column_width=True)
-        
+st.image(image, caption="Imagen cargada", use_container_width=True)        
         st.write("**Detecciones morfológicas preliminares:**")
         if net is not None:
             image_np = np.array(image.convert('RGB'))
